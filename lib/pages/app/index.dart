798b1c041aeb18b35a79db2af830ab0c94404a19
Mockingbirdcc <1768385508@qq.com>
2020-04-09 22:48:49 +0800

    整理代码

--- lib/pages/app/index.dart
+++ lib/pages/app/index.dart
@@ -70,109 +70,125 @@ class _App extends State<App> {
     print('didUpdateWidget');
   }
 
+  // 左侧抽屉
+  renderDrawer(context, _global) {
+    return SmartDrawer(
+      widthPercent: 0.6,
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          DrawerHeader(
+            decoration: BoxDecoration(
+              color: Theme.of(context).primaryColor,
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.only(left: 16),
+                  child:
+                      Text('换肤', style: Theme.of(context).textTheme.subtitle),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: MaterialColorPicker(
+                    elevation: 1,
+                    iconSelected: Icons.color_lens,
+                    allowShades: false,
+                    spacing: 16,
+                    onMainColorChange: (Color color) {
+                      _global.changeTheme(color);
+                    },
+                    selectedColor: _global.theme,
+                    colors: _global.colorList,
+                  ),
+                )
+              ],
+            ),
+          ),
+          SwitchListTile(
+            value: _global.isDark,
+            onChanged: (value) {
+              _global.changeThemeMode(value);
+            },
+            title: Text('暗黑模式'),
+            secondary: Icon(
+                _global.isDark ? Icons.invert_colors : Icons.invert_colors_off),
+            selected: _global.isDark,
+          ),
+        ],
+      ),
+    );
+  }
+
+  renderAppBar() {
+    return PreferredSize(
+      child: AppBar(
+        elevation: 1,
+        title: Container(
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                child: SearchTextFieldWidget(
+                  hintText: '搜索',
+                  margin: EdgeInsets.only(left: 0.0, right: 0.0),
+                  onTab: () {
+                    // Router.push(context, Router.searchPage, '电影/电视剧/影人');
+                  },
+                ),
+              ),
+            ],
+          ),
+          alignment: Alignment(0.0, 0.0),
+        ),
+      ),
+      preferredSize: Size.fromHeight(ScreenUtils.screenH(context) * 0.07),
+    );
+  }
+
+  renderBody() {
+    return IndexedStack(
+      index: _selectIndex,
+      children: _pageList,
+    );
+  }
+
+  renderBottomNavigationBar() {
+    return BottomNavigationBar(
+        items: itemList,
+        onTap: (int index) {
+          ///这里根据点击的index来显示，非index的page均隐藏，这步需要判断长度做到延时加载数据
+          if (index + 1 > _pageList.length) {
+            _pageList.add(Classify(
+              typeId: store.type.data[index].typeId,
+            ));
+          }
+          setState(() {
+            _selectIndex = index;
+          });
+        },
+        //图标大小
+        iconSize: 24,
+        //当前选中的索引
+        currentIndex: _selectIndex,
+        //选中后，底部BottomNavigationBar内容的颜色(选中时，默认为主题色)（仅当type: BottomNavigationBarType.fixed,时生效）
+        fixedColor: Theme.of(context).primaryColor,
+        type: BottomNavigationBarType.fixed,
+        selectedFontSize: 10,
+        unselectedFontSize: 10);
+  }
+
   @override
   Widget build(BuildContext context) {
     final Global _global = Provider.of<Global>(context);
     return store.isLoading
         ? Container()
         : Scaffold(
-            drawer: SmartDrawer(
-              widthPercent: 0.6,
-              child: ListView(
-                padding: EdgeInsets.zero,
-                children: <Widget>[
-                  DrawerHeader(
-                    decoration: BoxDecoration(
-                      color: Theme.of(context).primaryColor,
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Padding(
-                          padding: EdgeInsets.only(left: 16),
-                          child: Text('换肤',
-                              style: Theme.of(context).textTheme.subtitle),
-                        ),
-                        Expanded(
-                          flex: 1,
-                          child: MaterialColorPicker(
-                            elevation: 1,
-                            iconSelected: Icons.color_lens,
-                            allowShades: false,
-                            spacing: 16,
-                            onMainColorChange: (Color color) {
-                              _global.changeTheme(color);
-                            },
-                            selectedColor: _global.theme,
-                            colors: _global.colorList,
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                  SwitchListTile(
-                    value: _global.isDark,
-                    onChanged: (value) {
-                      _global.changeThemeMode(value);
-                    },
-                    title: Text('暗黑模式'),
-                    secondary: Icon(_global.isDark
-                        ? Icons.invert_colors
-                        : Icons.invert_colors_off),
-                    selected: _global.isDark,
-                  ),
-                ],
-              ),
-            ),
-            appBar: PreferredSize(
-                child: AppBar(
-                  elevation: 1,
-                  title: Container(
-                    child: Row(
-                      children: <Widget>[
-                        Expanded(
-                          child: SearchTextFieldWidget(
-                            hintText: '搜索',
-                            margin: EdgeInsets.only(left: 0.0, right: 0.0),
-                            onTab: () {
-                              // Router.push(context, Router.searchPage, '电影/电视剧/影人');
-                            },
-                          ),
-                        ),
-                      ],
-                    ),
-                    alignment: Alignment(0.0, 0.0),
-                  ),
-                ),
-                preferredSize:
-                    Size.fromHeight(ScreenUtils.screenH(context) * 0.07)),
-            body: IndexedStack(
-              index: _selectIndex,
-              children: _pageList,
-            ), // https://www.jianshu.com/p/86d29a939624
-            bottomNavigationBar: BottomNavigationBar(
-                items: itemList,
-                onTap: (int index) {
-                  ///这里根据点击的index来显示，非index的page均隐藏，这步需要判断长度做到延时加载数据
-                  if (index + 1 > _pageList.length) {
-                    _pageList.add(Classify(
-                      typeId: store.type.data[index].typeId,
-                    ));
-                  }
-                  setState(() {
-                    _selectIndex = index;
-                  });
-                },
-                //图标大小
-                iconSize: 24,
-                //当前选中的索引
-                currentIndex: _selectIndex,
-                //选中后，底部BottomNavigationBar内容的颜色(选中时，默认为主题色)（仅当type: BottomNavigationBarType.fixed,时生效）
-                fixedColor: Theme.of(context).primaryColor,
-                type: BottomNavigationBarType.fixed,
-                selectedFontSize: 10,
-                unselectedFontSize: 10),
+            drawer: renderDrawer(context, _global),
+            appBar: renderAppBar(),
+            body: renderBody(), // https://www.jianshu.com/p/86d29a939624
+            bottomNavigationBar: renderBottomNavigationBar(),
           );
   }
 }

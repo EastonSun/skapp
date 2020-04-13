d5a0c61160ab7a148db2dbf56b2e614333b109ee
ncer <1768385508@qq.com>
2020-04-13 16:55:54 +0800

    改icon

--- lib/utils/map.dart
+++ lib/utils/map.dart
@@ -7,15 +7,15 @@ var typeMap = {
   },
   'lianxuju': {
     'activeIcon': Icon(Icons.live_tv),
-    'normalIcon': Icon(Icons.tv),
+    'normalIcon': Icon(Icons.subscriptions),
   },
   'zongyi': {
     'activeIcon': Icon(Icons.library_music),
     'normalIcon': Icon(Icons.music_video),
   },
   'dongman': {
-    'activeIcon': Icon(Icons.leak_add),
-    'normalIcon': Icon(Icons.leak_remove),
+    'activeIcon': Icon(Icons.child_care),
+    'normalIcon': Icon(Icons.child_friendly),
   },
   'zixun': {
     'activeIcon': Icon(Icons.fiber_new),
